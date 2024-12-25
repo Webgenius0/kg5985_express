13d62328fdbb8b5718c70f8be397a2743b1c0062
sholaymanbdcalling <sholaymanbdcalling@gmail.com>
2024-12-25 14:06:48 +0600

    updated db

--- src/db/connectToMongoDB.js
+++ src/db/connectToMongoDB.js
@@ -1,31 +1,17 @@
 const mongoose = require("mongoose");
 const AppError = require("../utils/AppError");
 
-async function connectToMongoDB(req, res, next) {
+async function connectToMongoDB() {
     try {
-        const uri = process.env.MONGODB_URI;
-
-        if (!uri) {
-            return next(new AppError("MONGODB_URI is not defined in the environment variables.", 500));
-        }
-
-        const fullUri = `${uri}/reminderApp?retryWrites=true&w=majority`;
-
-        await mongoose.connect(fullUri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
+        const uri = `${process.env.MONGODB_URI}/reminderApp`;
+        await mongoose.connect(uri);
         console.log("Connected to MongoDB");
-
     } catch (error) {
         if (error instanceof AppError) {
             console.error(`AppError: ${error.message}`);
         } else {
             console.error("Unexpected Error connecting to MongoDB:", error.message);
         }
-
-        process.exit(1);
     }
 }
 
