4c4f591bf3d0035496575b3c67da0ed6affa0420
sholaymanbdcalling <sholaymanbdcalling@gmail.com>
2025-02-18 12:53:27 +0600

    fixed

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(mongoSanitize());
 app.use(limiter);
 
 
+
 //welcome to route
 app.get("/",(req,res)=>{
     res.status(200).json({message:"Welcome to KG5985"});
@@ -69,6 +70,7 @@ app.use((err, req, res, next) => {
 });
 
 // Call the async function to connect to MongoDB
+
 connectToMongoDB();
 
 module.exports = app;
